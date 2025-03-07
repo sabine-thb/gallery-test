e07f77d24a7cef5122459c823db89058c480c574
Thibout Sabine <124242397+sabine-thb@users.noreply.github.com>
2025-03-07 15:08:54 +0100

    calcul responsive width screenSize

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@
     <transition name="fade">
       <startScreen v-if="!started" @start="startExperience" />
     </transition>
+    <transition name="fade">
+      <controls v-if="started"  />
+    </transition>
     <div id="experience">
       <ThreeScene v-if="started"/>
     </div>
@@ -12,10 +15,12 @@
 
 
 
+
 <script setup>
 import { ref } from 'vue'
 import ThreeScene from './components/webglApplication/ThreeScene.vue'
 import startScreen from './components/startScreen/startScreen.vue'
+import controls from './components/controls/controls.vue'
 import soundButton from './components/soundButton/soundButton.vue'
 import './style.css'
 
@@ -24,6 +29,8 @@ const started = ref(false)
 const startExperience = () => {
   started.value = true
 }
+
+
 
 
 
