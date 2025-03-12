c275f60ddd7339657151160dd6d942b8a62fa39c
Andy <124242004+Rabarison-Andy@users.noreply.github.com>
2025-03-12 15:34:14 +0100

    Delete .nuxt directory

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-export { default } from "file:///C:/Users/Andry/Desktop/Cours%20MMI/MMI3/Garbage/Gallery/gallery-test/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
-=======
-export { default } from "file:///Users/admin/MMI3/PROJET/gallery-test/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
->>>>>>> main
