fe63b37d363cdf361449e635653bdc7a9414af24
Andy <124242004+Rabarison-Andy@users.noreply.github.com>
2025-03-12 15:36:27 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,64 +3,40 @@
     <transition name="fade">
       <startScreen v-if="!started" @start="startExperience" />
     </transition>
+    <transition name="fade">
+      <controls v-if="started" />
+    </transition>
     <div id="experience">
-      <ThreeScene v-if="started"/>
-      <div
-        class="sound-button"
-        @click.stop="toggleMute"
-        :class="{ muted: isMuted }"
-      >
-        <div class="sound-button-line"></div>
-        <div class="sound-button-line"></div>
-        <div class="sound-button-line"></div>
-        <div class="sound-button-line"></div>
-        <div class="sound-button-line"></div>
-      </div>
+      <ThreeScene v-if="started" @experience-ready="handleExperienceReady" />
     </div>
+    <soundButton v-if="started" @click="toggleMute" :isMuted="isMuted" />
   </div>
-  <soundButton v-if="started" @click="toggleMute" :isMuted="isMuted"/>
 </template>
 
-<<<<<<< Updated upstream
-
-
-
-=======
->>>>>>> Stashed changes
 <script setup>
-import { ref } from 'vue'
-import ThreeScene from './components/webglApplication/ThreeScene.vue'
-import startScreen from './components/startScreen/startScreen.vue'
-import controls from './components/controls/controls.vue'
-import soundButton from './components/soundButton/soundButton.vue'
-import './style.css'
+import { ref } from 'vue';
+import ThreeScene from './components/webglApplication/ThreeScene.vue';
+import startScreen from './components/startScreen/startScreen.vue';
+import controls from './components/controls/controls.vue';
+import soundButton from './components/soundButton/soundButton.vue';
+import './style.css';
 
-<<<<<<< Updated upstream
-const started = ref(false)
-const isMuted = ref(false)
-let audioElement = null
+const started = ref(false);
+const isMuted = ref(false);
+const experience = ref(null); // Référence à l'instance de Experience
 
-
-// Démarre l'expérience
 const startExperience = () => {
-  started.value = true
-  // Créer et jouer l'audio
-  audioElement = new Audio('/audio/song.wav')
-  audioElement.loop = true
-  audioElement.play()
-}
+  started.value = true;
+};
+
+const handleExperienceReady = (exp) => {
+  experience.value = exp; // Stocker l'instance de Experience
+};
 
 const toggleMute = () => {
-  if (audioElement) {
-    isMuted.value = !isMuted.value
-    audioElement.muted = isMuted.value
+  isMuted.value = !isMuted.value;
+  if (experience.value) {
+    experience.value.toggleMuteSound(isMuted.value); // Appeler la méthode de Experience
   }
-}
-
-=======
-// Change l'état du mute
-const toggleMute = () => {
-  isMuted.value = !isMuted.value
-}
->>>>>>> Stashed changes
+};
 </script>
